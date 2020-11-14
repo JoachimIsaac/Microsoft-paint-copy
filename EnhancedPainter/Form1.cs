f6bc7dde7d88d184a61b2e224f39da72c53be220
joachim <joachimvivian3@gmail.com>
2020-11-14 00:47:06 -0600

    commented stuff need to do review and to add try catches and error checks . review rubric too

--- EnhancedPainter/Form1.cs
+++ EnhancedPainter/Form1.cs
@@ -17,18 +17,12 @@ namespace EnhancedPainter
     public partial class PainterForm : Form
     {
         public Graphics graphics;
-        private List<Rectangle> ListOfRectangles = new List<Rectangle>();
-        private List<Rectangle> ListRecsForEllipse = new List<Rectangle>();
-        private Pen pen = new Pen(Color.Black, 4.0F);
-        private Rectangle rect = new Rectangle(0, 0, 100, 50);
         private ShapesBuilder SBuilder;
 
 
 
+        public PainterForm(){
 
-        public PainterForm()
-        {
-            ////////
             InitializeComponent();
             graphics = Canvaspanel.CreateGraphics();
             SBuilder = new ShapesBuilder(graphics);
@@ -41,11 +35,11 @@ namespace EnhancedPainter
         //Clears the Canvas.
         private void Clearbutton_Click(object sender, EventArgs e)
         {
-            ///////
             Canvaspanel.Invalidate();
             SBuilder.ClearListsOfShapes();
- 
         }
+
+
 
 
         private void HideDimensionsSection()
@@ -55,7 +49,11 @@ namespace EnhancedPainter
             Height_label.Visible = false;
             Width_textBox.Visible = false;
             Height_textBox.Visible = false;
+            
         }
+
+
+
 
         private void RevealDimensionsSection()
         {
@@ -64,7 +62,12 @@ namespace EnhancedPainter
             Height_label.Visible = true;
             Width_textBox.Visible = true;
             Height_textBox.Visible = true;
+            Width_textBox.Text = "";
+            Height_textBox.Text = "";
         }
+
+
+
 
         private void Canvaspanel_MouseDown(object sender, MouseEventArgs e)
         {
@@ -75,7 +78,6 @@ namespace EnhancedPainter
             }
             else if (RectangleradioButton.Checked)
             {
-
                 SBuilder.setCurrentStartingState(true);
 
                 if (SBuilder.isStarting() == true)
@@ -99,22 +101,33 @@ namespace EnhancedPainter
 
 
         private void DrawLineOnCanvas(MouseEventArgs e)
-        {//////////
+        {
             if (SBuilder.isStarting() == true)
             {
                 SBuilder.setSPoint(new Point(e.X, e.Y));
+
                 SBuilder.setCurrentStartingState(false);
+
                 MouseLoactionlabel.Text = $"Starting point X:{SBuilder.getSPoint().X} | Y:{SBuilder.getSPoint().Y}";
             }
             else
             {
                 SBuilder.setEPoint(new Point(e.X, e.Y));
+
                 SBuilder.setCurrentStartingState(true);
+
                 SBuilder.DrawALine();
-                SBuilder.AddLineToListOfLines(new Tuple<Point, Point>(SBuilder.getSPoint(), SBuilder.getEPoint()));
+
+                Tuple<Pen, Point, Point> pen_and_points = new Tuple<Pen, Point, Point>((Pen)SBuilder.getPen().Clone(), SBuilder.getSPoint(), SBuilder.getEPoint());
+
+                SBuilder.AddLineToListOfLines(pen_and_points);
+
                 MouseLoactionlabel.Text = $"Last Ending point X:{SBuilder.getEPoint().X} | Y:{SBuilder.getEPoint().Y}";
             }
         }
+
+
+
 
         private void DrawRectangleOnCanvas(MouseEventArgs e)
         {
@@ -122,39 +135,31 @@ namespace EnhancedPainter
             if (SBuilder.isStarting() == true)
             {
                 SBuilder.setSPoint(new Point(e.X, e.Y));
+
                 SBuilder.setCurrentStartingState(true);
+
                 Tuple<int, int> dimnesions = GetRectangleDimensions();
+
                 SBuilder.DrawNewRectangle(dimnesions.Item1,dimnesions.Item2);
-                SBuilder.AddRectangleToListOfRectangles();
+
                 MouseLoactionlabel.Text = $"Last Ending point X:{SBuilder.getEPoint().X} | Y:{SBuilder.getEPoint().Y}";
-                //MouseLoactionlabel.Text = $"Starting point X:{SBuilder.getSPoint().X} | Y:{SBuilder.getSPoint().Y}";
-
             }
-
         }
 
         private void DrawOvalOnCanvas(MouseEventArgs e)
         {
-            if (SizeSmallradioButton.Checked)
+            if (SBuilder.isStarting() == true)
             {
-                rect.X = e.X;
-                rect.Y = e.Y;
-                graphics.DrawEllipse(pen, rect);
-            }
-            else if (SizeMediumradioButton.Checked)
-            {
-                rect.X = e.X;
-                rect.Y = e.Y;
-                graphics.DrawEllipse(pen, rect);
-            }
-            else
-            {
-                rect.X = e.X;
-                rect.Y = e.Y;
-                graphics.DrawEllipse(pen, rect);
-            }
+                SBuilder.setSPoint(new Point(e.X, e.Y));
 
-            ListRecsForEllipse.Add(rect);
+                SBuilder.setCurrentStartingState(true);
+
+                Tuple<int, int> dimnesions = GetRectangleDimensions();
+
+                SBuilder.DrawNewEllipse(dimnesions.Item1, dimnesions.Item2);
+
+                MouseLoactionlabel.Text = $"Last Ending point X:{SBuilder.getEPoint().X} | Y:{SBuilder.getEPoint().Y}";
+            }
         }
 
 
@@ -167,22 +172,20 @@ namespace EnhancedPainter
             }
             else if (RectangleradioButton.Checked)
             {
-            
                 ChangePenWidth(sender);
             }
             else if (OvalradioButton.Checked)
             {
-              
                 ChangePenWidth(sender);
             }
         }
 
 
+
+
+
         private Tuple<int, int>  GetRectangleDimensions()
         {
-
-            
-
             if (Width_textBox.Text.Length != 0 && Height_textBox.Text.Length != 0)
             {
                 int width = int.Parse(Width_textBox.Text);
@@ -195,43 +198,38 @@ namespace EnhancedPainter
 
 
         private void ChangePenWidth(Object sender)
-        {/////////////
+        {
             string checkedName = ((RadioButton)sender).Name;
 
             if (checkedName == "SizeSmallradioButton")
             {
-            
                 SBuilder.setPenWidth(4.0F);
             }
             else if (checkedName == "SizeMediumradioButton")
             {
-        
                 SBuilder.setPenWidth(10.0F);
             }
             else
             {
-
                 SBuilder.setPenWidth(20.0F);
-        
             }
         }
 
 
 
-       
 
-
+        //Actives the button 
         private void ChooseColorbutton_Click(object sender, EventArgs e)
-        {/////////////
+        {
             OpenColorChooser();
         }
 
 
 
+
+        //Open's color chooser dialog box.
         private void OpenColorChooser()
         {
-            ////////////////////
-            //Try catch 
             ColorDialog colorDlg = new ColorDialog();
             colorDlg.AllowFullOpen = false;
             colorDlg.AnyColor = true;
@@ -240,48 +238,50 @@ namespace EnhancedPainter
 
             if (colorDlg.ShowDialog() == DialogResult.OK)
             {
-                //pen.Color = colorDlg.Color;
                 SBuilder.setPenColor(colorDlg.Color);
                 ColorIndicatorlabel.BackColor = colorDlg.Color;
             }
         }
 
+
+
+        //Redraws the canvas when the paint event is triggered.
         private void Canvaspanel_paint(object sender, PaintEventArgs e)
-        {//////////////
+        {
             SBuilder.RedrawCanvas();
         }
 
+
+
+
+
+        //When the shape radio button is changed, hide dimension's group box and it's content's or reveal them.
         private void ShapeCheckChange(object sender, EventArgs e)
         {
             if (LineradioButton.Checked)
             {
                 HideDimensionsSection();
             }
-            else if (RectangleradioButton.Checked)
+            else
             {
                 RevealDimensionsSection();
             }
-            else if (OvalradioButton.Checked)
-            {
-                RevealDimensionsSection();
-            }
+            
         }
 
 
-        //Prevents user from inputting anything but integers and floats, flaot only to one decimal point.
+
+        //Prevents user from inputting anything but positive integers.
         private void TextboxKeyPress(object sender, KeyPressEventArgs e)
         {
-            ///Allow only integers
+            
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
             {
                 e.Handled = true;
             }
-
-          
-            
         }
 
-       
+
     }
 }
 
@@ -289,163 +289,190 @@ namespace EnhancedPainter
 public class ShapesBuilder : Form
     {
         public Graphics graphics;
+
         public bool StartingPoint = true;
-        private List<Tuple<Point, Point>> ListOfLinePoints = new List<Tuple<Point, Point>>();
-        private List<Rectangle> ListOfRectangles = new List<Rectangle>();
-        private List<Rectangle> ListRecsForEllipse = new List<Rectangle>();
+
         private Point SPoint;
         private Point EPoint;
+
         private Pen pen = new Pen(Color.Black, 4.0F);
         private Rectangle rect = new Rectangle();
 
+        private List<Tuple<Pen,Point, Point>> ListOfLinePoints = new List<Tuple<Pen,Point, Point>>();
 
-        public ShapesBuilder(Graphics g)
-        {
+        private List<Tuple<Pen,Rectangle>> ListOfRectangles = new List<Tuple<Pen, Rectangle>>();
+
+        private List<Tuple<Pen, Rectangle>> ListRecsForEllipse = new List<Tuple<Pen, Rectangle>>();
+
+
+        //Constructor 
+        public ShapesBuilder(Graphics g){
             this.graphics = g;
         }
 
 
-
-        public void ClearListsOfShapes()
+        //Gets current pen
+        public Pen getPen()
         {
+            return this.pen;
+        }
+
+        //Sets pen's variable with a pen object
+        public void setPen(Pen p)
+        {
+            this.pen = p;
+        }
+
+        //Sets pen's color 
+        public void setPenColor(Color c)
+        {
+            this.pen.Color = c;
+        }
+
+        //Gets pen color 
+        public Color getPenColor()
+        {
+            return this.pen.Color;
+        }
+
+        //Sets pen's width.
+         public void setPenWidth(float width)
+        {
+            this.pen.Width = width;
+        }
+
+        
+        //Gets pen's current width.
+        public float getPenWidth()
+        {
+            return this.pen.Width;
+        }
+
+        //Sets the starting point.
+        public void setSPoint(Point p1)
+        {
+            SPoint = p1;
+        }
+
+
+        //Gets the starting point.
+        public Point getSPoint()
+        {
+            return SPoint;
+        }
+
+
+        //Sets the setting point.
+        public void setEPoint(Point p1)
+        {
+            EPoint = p1;
+        }
+
+
+        //Gets the setting point.
+        public Point getEPoint()
+        {
+            return EPoint;
+        }
+
+
+        //Returns true if it is a starting point.
+        public bool isStarting()
+        {
+                return StartingPoint;
+         }
+
+        
+        //Clears all the points in our point lists.
+        public void ClearListsOfShapes(){
+
             ListOfLinePoints.Clear();
             ListOfRectangles.Clear();
             ListRecsForEllipse.Clear();
+
         }
 
-        public bool isStarting()
-        {
-            return StartingPoint;
-        }
-
+        
+        //Sets the current starting state, either true or false.
         public void setCurrentStartingState(bool state)
         {
             StartingPoint = state;
         }
 
 
-        public void setSPoint(Point p1)
-        {
-            SPoint = p1;
-        }
-
-        public Point getSPoint()
-        {
 
 
-
-
-            return SPoint;
-
-        }
-
-
-
-        public void setEPoint(Point p1)
-        {
-            EPoint = p1;
-        }
-
-        public Point getEPoint()
-        {
-
-
-            return EPoint;
-
-        }
-
-
-        public void DrawALine()
-        {
-            this.graphics.DrawLine(this.pen, this.SPoint, this.EPoint);
-        }
-
-        public void AddLineToListOfLines(Tuple<Point, Point> points)
+    
+        //Adds points to list of points.
+        public void AddLineToListOfLines(Tuple<Pen,Point, Point> points)
         {
             this.ListOfLinePoints.Add(points);
         }
 
-        public void AddRectangleToListOfRectangles()
-        {
 
-            this.ListOfRectangles.Add(this.rect);
+        //Draws a line on the canvas 
+        public void DrawALine()
+        {
+            this.graphics.DrawLine((Pen)this.pen.Clone(), this.SPoint, this.EPoint);
         }
 
-        
 
+
+        //Draws rectangle on canvas
         public void DrawNewRectangle(int width, int height)
         {
+            Size size = new Size(width, height);
 
-        
-        //this.graphics.DrawRectangle(this.pen, this.rect);
-        Size size = new Size((int)width, (int)height);
+            this.rect = new Rectangle(SPoint, size);
 
-        this.rect = new Rectangle(SPoint, size);
-        this.graphics.DrawRectangle(this.pen, this.rect);
-        this.ListOfRectangles.Add(rect);
-
-    }
-
-
-
-        public void AddEllipseToListOfEllipses(Rectangle rect)
-        {
-
-            this.ListRecsForEllipse.Add(rect);
-
+            this.graphics.DrawRectangle(this.pen, this.rect);
+     
+            this.ListOfRectangles.Add(new Tuple<Pen,Rectangle>((Pen)this.pen.Clone(),this.rect));
         }
 
 
+        //Draws Ellipse on canvas
+        public void DrawNewEllipse(int width, int height)
+        {
+            Size size = new Size(width, height);
+
+            this.rect = new Rectangle(SPoint, size);
+
+            this.graphics.DrawEllipse(this.pen, this.rect);
+
+            this.ListRecsForEllipse.Add(new Tuple<Pen, Rectangle>((Pen)this.pen.Clone(), this.rect));
+        }
+
+
+
+        //Redraws Canvas of all shapes that was are currently drawn.
         public void RedrawCanvas()
         {
             if (this.ListOfLinePoints.Count != 0)
             {
                 foreach (var pointPair in this.ListOfLinePoints)
                 {
-                    graphics.DrawLine(pen, pointPair.Item1, pointPair.Item2);
+                    graphics.DrawLine(pointPair.Item1, pointPair.Item2, pointPair.Item3);
                 }
             }
 
             if (this.ListOfRectangles.Count != 0)
             {
-                foreach (var rectangle in this.ListOfRectangles)
+                foreach (var pen_and_rect in this.ListOfRectangles)
                 {
-                    graphics.DrawRectangle(pen, rectangle);
+                    graphics.DrawRectangle(pen_and_rect.Item1, pen_and_rect.Item2);
                 }
             }
 
             if (this.ListRecsForEllipse.Count != 0)
             {
-                foreach (var rectangle in this.ListRecsForEllipse)
+                foreach (var pen_and_rect in this.ListRecsForEllipse)
                 {
-                    graphics.DrawEllipse(pen, rectangle);
+                   graphics.DrawEllipse(pen_and_rect.Item1, pen_and_rect.Item2);
                 }
             }
         }
 
-
-
-        public void setPenColor(Color c)
-        {
-            this.pen.Color = c;
-        }
-
-        public Color getPenColor()
-        {
-            return this.pen.Color;
-        }
-
-
-
-        public void setPenWidth(float width)
-        {
-            this.pen.Width = width;
-        }
-
-        public float getPenWidth()
-        {
-            return this.pen.Width;
-        }
 
         
     }
